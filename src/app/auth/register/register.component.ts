0b5f496fd29aca81b7e67b2ee312529e3b163d60
NicolasODT <nicolas.terwagne@gmail.com>
2024-04-24 14:02:12 +0200

    refactor: Update register component form validation and error handling

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -18,26 +18,30 @@ export class RegisterComponent {
   };
   confirmPassword: string = '';
 
-  constructor(private authService: AuthService, private router: Router,private toastr: ToastrService) {}
+  constructor(private authService: AuthService, private router: Router, private toastr: ToastrService) {}
 
   onSubmit(): void {
-    if (this.user.password === this.confirmPassword) {
-      this.authService.register(this.user).subscribe({
-        next: (response) => {
-          console.log('Inscription réussie', response);
-          // Redirection vers la page d'accueil après inscription
-          this.toastr.success('Inscription réussie');
-          this.router.navigate(['/home']);
-        },
-        error: (error) => {
-          console.error("Erreur lors de l'inscription", error);
-          this.toastr.error('Erreur lors de l\'inscription');
-        }
-      });
-    } else {
-      console.error('Les mots de passe ne correspondent pas.');
-      this.toastr.error('Les mots de passe ne correspondent pas.');
-
+    // Vérifie si le formulaire est valide et que les mots de passe correspondent
+    if (!this.isFormValid()) {
+      this.toastr.error('Veuillez corriger les erreurs dans le formulaire.');
+      return;
     }
+
+    // Appel au service d'authentification pour enregistrer le nouvel utilisateur
+    this.authService.register(this.user).subscribe({
+      next: (response) => {
+        this.toastr.success('Inscription réussie');
+        this.router.navigate(['/home']); // Redirection vers la page d'accueil après inscription réussie
+      },
+      error: (error) => {
+        console.error("Erreur lors de l'inscription", error);
+        this.toastr.error('Erreur lors de l\'inscription');
+      }
+    });
+  }
+
+  // Fonction pour vérifier si le formulaire est valide et que les mots de passe correspondent
+  isFormValid(): boolean {
+    return this.confirmPassword === this.user.password;
   }
 }
